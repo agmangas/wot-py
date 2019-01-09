306cbce348d8d0395e0d7e991d45f0b3e1acc976
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2019-01-09 19:57:10 +0100

    Schedule all messages in the background as soon as they arrive without yielding them

--- wotpy/protocols/mqtt/runner.py
+++ wotpy/protocols/mqtt/runner.py
@@ -25,7 +25,6 @@ class MQTTHandlerRunner(object):
     MQTT broker, delivers messages, and runs the handler in a loop."""
 
     DEFAULT_TIMEOUT_DELIVER_SECS = 0.1
-    DEFAULT_MESSAGES_BATCH_SIZE = 50
 
     DEFAULT_CLIENT_CONFIG = {
         "keep_alive": 10,
@@ -36,13 +35,11 @@ class MQTTHandlerRunner(object):
 
     def __init__(self, broker_url, mqtt_handler,
                  timeout_deliver_secs=DEFAULT_TIMEOUT_DELIVER_SECS,
-                 messages_batch_size=DEFAULT_MESSAGES_BATCH_SIZE,
                  reconnect_on_handler_error=True,
                  client_config=None):
         self._broker_url = broker_url
         self._mqtt_handler = mqtt_handler
         self._timeout_deliver_secs = timeout_deliver_secs
-        self._messages_batch_size = messages_batch_size
         self._reconnect_on_handler_error = reconnect_on_handler_error
         self._client_config = client_config if client_config else self.DEFAULT_CLIENT_CONFIG
         self._client = None
@@ -51,15 +48,20 @@ class MQTTHandlerRunner(object):
         self._event_stop_request = tornado.locks.Event()
         self._logr = logging.getLogger(__name__)
 
+    def _log(self, level, msg, **kwargs):
+        """Helper function to wrap all log messages."""
+
+        self._logr.log(level, "{} - {}".format(self._mqtt_handler.__class__.__name__, msg), **kwargs)
+
     @tornado.gen.coroutine
     def _connect(self):
         """MQTT connection helper function."""
 
-        self._logr.debug("Using MQTT client config: {}".format(self._client_config))
+        self._log(logging.DEBUG, "Using MQTT client config: {}".format(self._client_config))
 
         hbmqtt_client = MQTTClient(config=self._client_config)
 
-        self._logr.debug("Connecting MQTT client to broker: {}".format(self._broker_url))
+        self._log(logging.DEBUG, "Connecting MQTT client to broker: {}".format(self._broker_url))
 
         ack_con = yield hbmqtt_client.connect(self._broker_url)
 
@@ -67,7 +69,7 @@ class MQTTHandlerRunner(object):
             raise ConnectException("Error code in connection ACK: {}".format(ack_con))
 
         if self._mqtt_handler.topics:
-            self._logr.debug("Subscribing to: {}".format(self._mqtt_handler.topics))
+            self._log(logging.DEBUG, "Subscribing to: {}".format(self._mqtt_handler.topics))
             ack_sub = yield hbmqtt_client.subscribe(self._mqtt_handler.topics)
 
             if MQTTCodesACK.SUB_ERROR in ack_sub:
@@ -80,15 +82,15 @@ class MQTTHandlerRunner(object):
         """MQTT disconnection helper function."""
 
         try:
-            self._logr.debug("Disconnecting MQTT client")
+            self._log(logging.DEBUG, "Disconnecting MQTT client")
 
             if self._mqtt_handler.topics:
-                self._logr.debug("Unsubscribing from: {}".format(self._mqtt_handler.topics))
+                self._log(logging.DEBUG, "Unsubscribing from: {}".format(self._mqtt_handler.topics))
                 yield self._client.unsubscribe([name for name, qos in self._mqtt_handler.topics])
 
             yield self._client.disconnect()
         except Exception as ex:
-            self._logr.warning("Error disconnecting MQTT client: {}".format(ex), exc_info=True)
+            self._log(logging.WARNING, "Error disconnecting MQTT client: {}".format(ex), exc_info=True)
         finally:
             self._client = None
 
@@ -119,19 +121,12 @@ class MQTTHandlerRunner(object):
         """Listens and processes the next published message.
         It will wait for a finite amount of time before desisting."""
 
-        msgs = []
-
         try:
-            while len(msgs) < self._messages_batch_size:
-                msg = yield self._client.deliver_message(timeout=self._timeout_deliver_secs)
-                msgs.append(msg)
+            msg = yield self._client.deliver_message(timeout=self._timeout_deliver_secs)
+            self._log(logging.DEBUG, "Handling message: {}".format(msg.data))
+            tornado.gen.convert_yielded(self._mqtt_handler.handle_message(msg))
         except asyncio.TimeoutError:
             pass
-
-        if not len(msgs):
-            return
-
-        yield [self._mqtt_handler.handle_message(msg) for msg in msgs]
 
     @tornado.gen.coroutine
     def publish_queued_messages(self):
@@ -160,12 +155,13 @@ class MQTTHandlerRunner(object):
             yield self.handle_delivered_message()
             yield self.publish_queued_messages()
         except Exception as ex:
-            self._logr.warning(
+            self._log(
+                logging.WARNING,
                 "MQTT handler error ({}): {}".format(self._mqtt_handler.__class__, ex),
                 exc_info=True)
 
             if self._reconnect_on_handler_error:
-                self._logr.warning("Attempting to reconnect MQTT client")
+                self._log(logging.WARNING, "Attempting to reconnect MQTT client")
                 yield self.connect(force_reconnect=True)
 
     def _add_loop_callback(self):
@@ -180,7 +176,7 @@ class MQTTHandlerRunner(object):
                     while not self._event_stop_request.is_set():
                         yield self.handler_loop_iter()
             except tornado.util.TimeoutError:
-                self._logr.warning("Attempted to start an MQTT handler loop when another was already running")
+                self._log(logging.WARNING, "Cannot start MQTT handler loop while another is already running")
 
         tornado.ioloop.IOLoop.current().spawn_callback(run_loop)
 
