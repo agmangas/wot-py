07298cf3b0d7589e9bddc7d1a4accf8f262d1e78
Andres Garcia Mangas <agmangas@gmail.com>
2019-01-13 20:35:11 +0100

    Handle reconnection ourselves

--- wotpy/protocols/mqtt/runner.py
+++ wotpy/protocols/mqtt/runner.py
@@ -8,6 +8,7 @@ Base class for MQTT handlers.
 import asyncio
 import datetime
 import logging
+import uuid
 
 import tornado.concurrent
 import tornado.gen
@@ -24,10 +25,11 @@ class MQTTHandlerRunner(object):
     MQTT broker, delivers messages, and runs the handler in a loop."""
 
     DEFAULT_TIMEOUT_SECS = 0.1
+    DEFAULT_SLEEP_ERR_RECONN = 2.0
 
     DEFAULT_CLIENT_CONFIG = {
         "keep_alive": 10,
-        "auto_reconnect": True,
+        "auto_reconnect": False,
         "default_qos": 0,
         "default_retain": False
     }
@@ -35,13 +37,16 @@ class MQTTHandlerRunner(object):
     def __init__(self, broker_url, mqtt_handler,
                  timeout_deliver_secs=DEFAULT_TIMEOUT_SECS,
                  timeout_get_queue_secs=DEFAULT_TIMEOUT_SECS,
+                 sleep_error_reconnect=DEFAULT_SLEEP_ERR_RECONN,
                  client_config=None):
         self._broker_url = broker_url
         self._mqtt_handler = mqtt_handler
         self._timeout_deliver_secs = timeout_deliver_secs
         self._timeout_get_queue_secs = timeout_get_queue_secs
+        self._sleep_error_reconnect = sleep_error_reconnect
         self._client_config = client_config if client_config else self.DEFAULT_CLIENT_CONFIG
         self._client = None
+        self._client_id = uuid.uuid4().hex
         self._lock_conn = tornado.locks.Lock()
         self._lock_run = tornado.locks.Lock()
         self._event_stop_request = tornado.locks.Event()
@@ -56,13 +61,14 @@ class MQTTHandlerRunner(object):
     def _connect(self):
         """MQTT connection helper function."""
 
-        self._log(logging.DEBUG, "Using MQTT client config: {}".format(self._client_config))
+        self._log(logging.DEBUG, "MQTT client ID: {}".format(self._client_id))
+        self._log(logging.DEBUG, "MQTT client config: {}".format(self._client_config))
 
-        hbmqtt_client = MQTTClient(config=self._client_config)
+        hbmqtt_client = MQTTClient(client_id=self._client_id, config=self._client_config)
 
-        self._log(logging.DEBUG, "Connecting MQTT client to broker: {}".format(self._broker_url))
+        self._log(logging.INFO, "Connecting MQTT client to broker: {}".format(self._broker_url))
 
-        ack_con = yield hbmqtt_client.connect(self._broker_url)
+        ack_con = yield hbmqtt_client.connect(self._broker_url, cleansession=False)
 
         if ack_con != MQTTCodesACK.CON_OK:
             raise ConnectException("Error code in connection ACK: {}".format(ack_con))
@@ -89,7 +95,7 @@ class MQTTHandlerRunner(object):
 
             yield self._client.disconnect()
         except Exception as ex:
-            self._log(logging.WARNING, "Error disconnecting MQTT client: {}".format(ex), exc_info=True)
+            self._log(logging.DEBUG, "Error disconnecting MQTT client: {}".format(ex), exc_info=True)
         finally:
             self._client = None
 
@@ -99,6 +105,7 @@ class MQTTHandlerRunner(object):
 
         with (yield self._lock_conn.acquire()):
             if self._client is not None and force_reconnect:
+                self._log(logging.DEBUG, "Forcing reconnection")
                 yield self._disconnect()
             elif self._client is not None:
                 return
@@ -123,31 +130,49 @@ class MQTTHandlerRunner(object):
         while not self._event_stop_request.is_set():
             try:
                 msg = yield self._client.deliver_message(timeout=self._timeout_deliver_secs)
-                self._log(logging.DEBUG, "Handling message: {}".format(msg.data))
-                tornado.gen.convert_yielded(self._mqtt_handler.handle_message(msg))
+
+                try:
+                    self._log(logging.DEBUG, "Handling message: {}".format(msg.data))
+                    tornado.gen.convert_yielded(self._mqtt_handler.handle_message(msg))
+                except Exception as ex:
+                    self._log(logging.WARNING, "MQTT handler error: {}".format(ex), exc_info=True)
             except asyncio.TimeoutError:
                 pass
             except Exception as ex:
-                self._log(logging.WARNING, "MQTT handler error: {}".format(ex), exc_info=True)
+                self._log(logging.WARNING, "Error on MQTT deliver: {}".format(ex), exc_info=True)
+
+                try:
+                    yield tornado.gen.sleep(self._sleep_error_reconnect)
+                    yield self.connect(force_reconnect=True)
+                except Exception as ex:
+                    self._log(logging.ERROR, "Error reconnecting: {}".format(ex), exc_info=True)
 
     @tornado.gen.coroutine
     def _publish_queued_messages(self):
         """Gets the pending messages from the handler queue and publishes them on the broker."""
 
+        msg_publish = None
+
         while not self._event_stop_request.is_set():
             try:
-                msg_publish = yield self._mqtt_handler.queue.get(
-                    timeout=datetime.timedelta(seconds=self._timeout_get_queue_secs))
+                if msg_publish is None:
+                    msg_publish = yield self._mqtt_handler.queue.get(
+                        timeout=datetime.timedelta(seconds=self._timeout_get_queue_secs))
+                else:
+                    self._log(logging.WARNING, "Republish attempt: {}".format(msg_publish))
 
                 yield self._client.publish(
                     topic=msg_publish["topic"],
                     message=msg_publish["data"],
                     qos=msg_publish.get("qos", None),
                     retain=msg_publish.get("retain", None))
+
+                msg_publish = None
             except tornado.util.TimeoutError:
                 pass
             except Exception as ex:
                 self._log(logging.WARNING, "Exception publishing: {}".format(ex), exc_info=True)
+                yield tornado.gen.sleep(self._sleep_error_reconnect)
 
     def _add_loop_callback(self):
         """Adds the callback that will start the infinite loop
