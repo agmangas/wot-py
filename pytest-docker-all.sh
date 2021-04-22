9d700ce1376f19f6c8c772ee9d0704498a458961
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2021-04-22 16:59:46 +0200

    Add 3.9 to test matrix

--- pytest-docker-all.sh
+++ pytest-docker-all.sh
@@ -8,3 +8,4 @@ PYTHON_TAG="2.7" PYTEST_ARGS=${PYTEST_ARGS} ./pytest-docker.sh
 PYTHON_TAG="3.6" PYTEST_ARGS=${PYTEST_ARGS} ./pytest-docker.sh
 PYTHON_TAG="3.7" PYTEST_ARGS=${PYTEST_ARGS} ./pytest-docker.sh
 PYTHON_TAG="3.8" PYTEST_ARGS=${PYTEST_ARGS} ./pytest-docker.sh
+PYTHON_TAG="3.9" PYTEST_ARGS=${PYTEST_ARGS} ./pytest-docker.sh
