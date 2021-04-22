162c1b731b3d0f385cef30400fecd12b947bb51f
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2021-04-22 12:40:15 +0200

    Add script to test all versions using Docker

---
+++ pytest-docker-all.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -x
+
+: ${PYTEST_ARGS:="-v --disable-warnings"}
+
+PYTHON_TAG="2.7" PYTEST_ARGS=${PYTEST_ARGS} ./pytest-docker.sh
+PYTHON_TAG="3.6" PYTEST_ARGS=${PYTEST_ARGS} ./pytest-docker.sh
+PYTHON_TAG="3.7" PYTEST_ARGS=${PYTEST_ARGS} ./pytest-docker.sh
+PYTHON_TAG="3.8" PYTEST_ARGS=${PYTEST_ARGS} ./pytest-docker.sh
--- pytest-docker.sh
+++ pytest-docker.sh
@@ -3,23 +3,28 @@
 set -e
 set -x
 
-: ${TEMP_NAME:="wotpytest"}
 : ${PYTHON_TAG:="3.8"}
-: ${PYTEST_ARGS:=""}
+: ${PYTEST_ARGS:="-v"}
 
 CURR_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd )"
-TEMP_DIR=$(python -c "import tempfile; print(tempfile.gettempdir());")
+TEMP_BASE=$(python3 -c "import tempfile; print(tempfile.gettempdir());")
+TEMP_NAME=$(python3 -c "import uuid; print(\"wotpy-{}\".format(uuid.uuid4().hex));")
+TEMP_PATH=${TEMP_BASE}/${TEMP_NAME}
 
-rm -fr ${TEMP_DIR}/${TEMP_NAME}
-cp -r ${CURR_DIR} ${TEMP_DIR}/${TEMP_NAME}
-cd ${TEMP_DIR}/${TEMP_NAME}
+rm -fr ${TEMP_PATH}
+cp -r ${CURR_DIR} ${TEMP_PATH}
+cd ${TEMP_PATH}
 git clean -x -d -f
+
+PYTEST_EXIT_CODE=0
 
 docker run --rm -it \
 -v $(pwd):/app \
 -e WOTPY_TESTS_MQTT_BROKER_URL=${WOTPY_TESTS_MQTT_BROKER_URL} \
 python:${PYTHON_TAG} \
-/bin/bash -c "cd /app && pip install -U .[tests] && pytest -v ${PYTEST_ARGS}"
+/bin/bash -c "cd /app && pip install -U .[tests] && pytest ${PYTEST_ARGS}" || PYTEST_EXIT_CODE=$?
 
 cd ${CURR_DIR}
-rm -fr ${TEMP_DIR}/${TEMP_NAME}
+rm -fr ${TEMP_PATH}
+
+exit $PYTEST_EXIT_CODE
