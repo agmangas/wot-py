ea5790da632d633e455907923e6fa853ee512e37
Nikos Filinis <nfilinis@netmode.ntua.gr>
2026-08-04 18:49:12 +0300

    Fix copyright headers

--- examples/cpumonitor/server.py
+++ examples/cpumonitor/server.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2018 CTIC Centro Tecnologico
+# Copyright (c) 2025 National Technical University of Athens
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of
 # this software and associated documentation files (the "Software"), to deal in
--- pytest-docker.sh
+++ pytest-docker.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Copyright (c) 2021 CTIC Centro Tecnologico
+# Copyright (c) 2025 National Technical University of Athens
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of
 # this software and associated documentation files (the "Software"), to deal in
